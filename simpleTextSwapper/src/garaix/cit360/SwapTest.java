841ae111d088a2e356906d2ff154abd9c813487e
Davy <Davy@DESKTOP-3LNPV5L.Home>
2017-12-08 00:48:27 -0700

    Added more assert methods

--- simpleTextSwapper/src/garaix/cit360/SwapTest.java
+++ simpleTextSwapper/src/garaix/cit360/SwapTest.java
@@ -10,13 +10,20 @@ class SwapTest {
 	void swapTest() {
 		Swapper swap = new Swapper();
 		String phrase1 = swap.Swap("Today, {name} went to the store.", "{name}", "Davy");
-		assertEquals("Today, Davy went to the store.", phrase1, "{name} mustbe replaced with \"Davy");
+		String expectedResults1 = "Today, Davy went to the store.";
+		
+		assertEquals(expectedResults1, phrase1, "{name} mustbe replaced with \"Davy");
+		
+		// verifies that they aren't the same object
+		// In other words, it verifies that the test is actually using the method and not simply a repeat of the string.
+		// A test to verify the test
+		assertNotSame(expectedResults1, phrase1);
 		
 		String phrase2 = swap.Swap("La v{accent}rit{accent} est ensoleill{accent}e.", "{accent}", "é");
 		assertEquals("La vérité est ensoleillée.", phrase2, "All occurences must be replaced.");
 		
 		String phrase3 = swap.Swap("{number} plus {number} does not equal {number}.", "{number}", "two");
-		assertEquals("Two plus two does not equal two.", phrase3, "The initial occurence should have its first letter capitalized.");		
+		assertEquals("Two plus two does not equal two.", phrase3, "The initial occurence should have its first letter capitalized.");
 		
 	}
 
